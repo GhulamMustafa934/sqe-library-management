618775f018800ed8e8341a23b87ce4bbb487f865
Mustafa <lasharighulammustafa934@gmail.com>
2026-08-18 16:24:37 +0500

    feat: add borrow and return_book methods

--- src/book.py
+++ src/book.py
@@ -4,3 +4,13 @@ class Book:
         self.author = author
         self.isbn = isbn
         self.is_borrowed = False
+
+    def borrow(self) -> None:
+        if self.is_borrowed:
+            raise ValueError("Book is already borrowed")
+        self.is_borrowed = True
+
+    def return_book(self) -> None:
+        if not self.is_borrowed:
+            raise ValueError("Book is not borrowed")
+        self.is_borrowed = False
